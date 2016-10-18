fcbf2025af1f1dd0c4e208e3d613627f35affe74
AyonMKhan <mohtasimtamjeed@gmail.com>
2016-10-19 02:12:53 +0600

    added devicons and some more awesome stuff

--- js/index.js
+++ js/index.js
@@ -1,3 +1,25 @@
 ﻿$(document).ready(function(){
   $("#myCarousel").carousel();
 });
+
+// handle links with @href started with '#' only
+$(document).on('click', '.nav a[href^="#"]', function(e) {
+  //any href links starting with #, that are children of .nav, this distinction is important because there are hrefs with # in the carousel
+    // target element id
+    var id = $(this).attr('href');
+
+    // target element
+    var $id = $(id);
+    if ($id.length === 0) {
+        return;
+    }
+
+    // prevent standard hash navigation (avoid blinking in IE)
+    e.preventDefault();
+
+    // top position relative to the document
+    var pos = $(id).offset().top;
+
+    // animated top scrolling
+    $('body, html').animate({scrollTop: pos});
+});
