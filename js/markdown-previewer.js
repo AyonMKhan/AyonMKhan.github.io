835f05ff26dca6f7c19a1f2f888381d936269145
AyonMKhan <mohtasimtamjeed@gmail.com>
2017-03-05 21:22:58 +0600

    revision to markdown preview js file

--- js/markdown-previewer.js
+++ js/markdown-previewer.js
@@ -1,44 +1,62 @@
-"use strict";
+(function () {
+	"use strict";
 
-import React from 'react.min';
-import ReactDOM from 'react-dom.min';
-import marked from 'marked.min';
+	document.addEventListener("DOMContentLoaded", function () {
 
-var Container = React.createClass({
-  displayName: "Container",
 
-  getInitialState: function getInitialState() {
-    return {
-      text: "Heading\n=======\n\nSub-heading\n-----------\n \n### Another deeper heading\n \nParagraphs are separated\nby a blank line.\n\nLeave 2 spaces at the end of a line to do a  \nline break\n\nText attributes *italic*, **bold**, \n`monospace`, ~~strikethrough~~ .\n\nShopping list:\n\n  * apples\n  * oranges\n  * pears\n\nNumbered list:\n\n  1. apples\n  2. oranges\n  3. pears\n\nThe rain---not the reign---in\nSpain.\n\n *[Ayon](https://freecodecamp.com/AyonKhan)*"
-    };
-  },
 
-  txtChanged: function txtChanged(e) {
-    this.setState({
-      text: e.target.value
-    });
-  },
+		var Container = React.createClass({
+			displayName: "Container",
 
-  render: function render() {
-    return React.createElement(
-      "div",
-      null,
-      React.createElement("textarea", { className: "input", value: this.state.text, onChange: this.txtChanged }),
-      React.createElement(RenderedText, { text: this.state.text })
-    );
-  }
-});
+			getInitialState: function getInitialState() {
+				return {
+					text: "Heading\n=======\n\nSub-heading\n-----------\n \n### Another deeper heading\n \nParagraphs are separated\nby a blank line.\n\nLeave 2 spaces at the end of a line to do a  \nline break\n\nText attributes *italic*, **bold**, \n`monospace`, ~~strikethrough~~ .\n\nShopping list:\n\n  * apples\n  * oranges\n  * pears\n\nNumbered list:\n\n  1. apples\n  2. oranges\n  3. pears\n\nThe rain---not the reign---in\nSpain.\n\n *[Ayon](https://freecodecamp.com/AyonKhan)*"
+				};
+			},
 
-var RenderedText = React.createClass({
-  displayName: "RenderedText",
+			txtChanged: function txtChanged(e) {
+				this.setState({
+					text: e.target.value
+				});
+			},
 
-  markup: function markup() {
-    return { __html: marked(this.props.text, { sanitize: true, gfm: true }) };
-  },
+			render: function render() {
+				return React.createElement(
+					"div",
+					null,
+					React.createElement("textarea", {
+						className: "input",
+						value: this.state.text,
+						onChange: this.txtChanged
+					}),
+					React.createElement(RenderedText, {
+						text: this.state.text
+					})
+				);
+			}
+		});
 
-  render: function render() {
-    return React.createElement("div", { className: "rendered", dangerouslySetInnerHTML: this.markup() });
-  }
-});
+		var RenderedText = React.createClass({
+			displayName: "RenderedText",
 
-ReactDOM.render(React.createElement(Container, null), document.getElementById('app'));
+			markup: function markup() {
+				return {
+					__html: marked(this.props.text, {
+						sanitize: true,
+						gfm: true
+					})
+				};
+			},
+
+			render: function render() {
+				return React.createElement("div", {
+					className: "rendered",
+					dangerouslySetInnerHTML: this.markup()
+				});
+			}
+		});
+
+		ReactDOM.render(React.createElement(Container, null), document.getElementById('app'));
+	}); //end of Dom content loaded
+
+}()); //end of use strict function wrapping
