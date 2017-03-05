89233e5ce7d5a0bcc3fb4e63333496113b4c553b
AyonMKhan <mohtasimtamjeed@gmail.com>
2017-03-05 21:46:22 +0600

    added babel-standalone

--- js/markdown-previewer.js
+++ js/markdown-previewer.js
@@ -6,57 +6,46 @@
 
 
 		var Container = React.createClass({
-			displayName: "Container",
-
-			getInitialState: function getInitialState() {
-				return {
-					text: "Heading\n=======\n\nSub-heading\n-----------\n \n### Another deeper heading\n \nParagraphs are separated\nby a blank line.\n\nLeave 2 spaces at the end of a line to do a  \nline break\n\nText attributes *italic*, **bold**, \n`monospace`, ~~strikethrough~~ .\n\nShopping list:\n\n  * apples\n  * oranges\n  * pears\n\nNumbered list:\n\n  1. apples\n  2. oranges\n  3. pears\n\nThe rain---not the reign---in\nSpain.\n\n *[Ayon](https://freecodecamp.com/AyonKhan)*"
-				};
-			},
-
-			txtChanged: function txtChanged(e) {
-				this.setState({
-					text: e.target.value
-				});
-			},
-
-			render: function render() {
-				return React.createElement(
-					"div",
-					null,
-					React.createElement("textarea", {
-						className: "input",
-						value: this.state.text,
-						onChange: this.txtChanged
-					}),
-					React.createElement(RenderedText, {
-						text: this.state.text
-					})
-				);
+		  getInitialState: function() {
+			return {
+			  text: "Heading\n=======\n\nSub-heading\n-----------\n \n### Another deeper heading\n \nParagraphs are separated\nby a blank line.\n\nLeave 2 spaces at the end of a line to do a  \nline break\n\nText attributes *italic*, **bold**, \n`monospace`, ~~strikethrough~~ .\n\nShopping list:\n\n  * apples\n  * oranges\n  * pears\n\nNumbered list:\n\n  1. apples\n  2. oranges\n  3. pears\n\nThe rain---not the reign---in\nSpain.\n\n *[Ayon](https://freecodecamp.com/AyonKhan)*"
 			}
+		  },
+
+		  txtChanged: function(e) {
+			this.setState({
+			  text: e.target.value
+			})
+		  },
+
+
+		  render: function() {
+			return (
+			  <div>
+				<textarea className="input" value={this.state.text} onChange={this.txtChanged}/>
+				<RenderedText text={this.state.text}/>
+			  </div>
+			);
+		  }
 		});
 
 		var RenderedText = React.createClass({
-			displayName: "RenderedText",
+		  markup: function() {
+			return {__html: marked(this.props.text, {sanitize: true, gfm: true})}
+		  },
 
-			markup: function markup() {
-				return {
-					__html: marked(this.props.text, {
-						sanitize: true,
-						gfm: true
-					})
-				};
-			},
+		  render: function() {
+			return (
+			  <div className="rendered" dangerouslySetInnerHTML={this.markup()}>
+			  </div>
+			)
+		  }
+		})
 
-			render: function render() {
-				return React.createElement("div", {
-					className: "rendered",
-					dangerouslySetInnerHTML: this.markup()
-				});
-			}
-		});
-
-		ReactDOM.render(React.createElement(Container, null), document.getElementById('app'));
+		ReactDOM.render(
+		  <Container />,
+		  document.getElementById('app')
+		);
 	}); //end of Dom content loaded
 
 }()); //end of use strict function wrapping
